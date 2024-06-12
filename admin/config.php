96971236cc0c0ed3f9f243d8ba24939e3535d362
pankaj <ramchandanipankaj787@gmail.com>
2024-06-12 11:55:53 +0530

    initial setup

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/opencart/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/opencart/system/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/opencart/image/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/opencart/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/opencart/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/opencart/system/config/');
+define('DIR_CACHE', '/opt/lampp/htdocs/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/opencart/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/htdocs/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
